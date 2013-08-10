28a766852118488cb097a987c6e493fba74ce96d
jon.chambers@gmail.com <jon.chambers@gmail.com@b68fe964-5755-0410-a06c-9fee2ea08261>
2013-08-10 04:03:08 +0000

    Added a test case to cover https://code.google.com/p/json-simple/issues/detail?id=87.

--- trunk/src/test/java/org/json/simple/JSONValueTest.java
+++ trunk/src/test/java/org/json/simple/JSONValueTest.java
@@ -230,4 +230,27 @@ public class JSONValueTest extends TestCase {
 		JSONValue.writeJSONString(new Object[] { "Hello", new Integer(12), new int[] { 1, 2, 3} }, writer);
 		assertEquals("[\"Hello\",12,[1,2,3]]", writer.toString());
 	}
+	
+	public void testArraysOfArrays() throws IOException {
+		
+		StringWriter writer;
+		
+		final int[][][] nestedIntArray = new int[][][]{{{1}, {5}}, {{2}, {6}}};
+		final String expectedNestedIntString = "[[[1],[5]],[[2],[6]]]";
+		
+		assertEquals(expectedNestedIntString, JSONValue.toJSONString(nestedIntArray));
+		
+		writer = new StringWriter();
+		JSONValue.writeJSONString(nestedIntArray, writer);
+		assertEquals(expectedNestedIntString, writer.toString());
+
+		final String[][] nestedStringArray = new String[][]{{"a", "b"}, {"c", "d"}};
+		final String expectedNestedStringString = "[[\"a\",\"b\"],[\"c\",\"d\"]]";
+		
+		assertEquals(expectedNestedStringString, JSONValue.toJSONString(nestedStringArray));
+		
+		writer = new StringWriter();
+		JSONValue.writeJSONString(nestedStringArray, writer);
+		assertEquals(expectedNestedStringString, writer.toString());
+	}
 }
