acd37cff1269156b9bcba65fb82286b56679b65f
István Boros <boros.istvan@outlook.com>
2017-09-26 15:39:00 +0200

    Replaced StringBuffer by StringBuilder

--- src/main/java/org/json/simple/parser/Yytoken.java
+++ src/main/java/org/json/simple/parser/Yytoken.java
@@ -26,7 +26,7 @@ public class Yytoken {
 	}
 	
 	public String toString(){
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		switch(type){
 		case TYPE_VALUE:
 			sb.append("VALUE(").append(value).append(")");
