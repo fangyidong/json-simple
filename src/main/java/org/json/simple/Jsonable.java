30b0bdc5a52d59a6cc37f9cc62ba67164d137127
Davin Loegering <davin.loegering@cliftonlabs.com>
2017-10-04 14:01:45 -0500

    Source cleanup.

--- src/main/java/org/json/simple/Jsonable.java
+++ src/main/java/org/json/simple/Jsonable.java
@@ -16,12 +16,12 @@ import java.io.Writer;
 /** Jsonables can be serialized in java script object notation (JSON).
  * @since 2.0.0 */
 public interface Jsonable{
-    /** Serialize to a JSON formatted string.
-     * @return a string, formatted in JSON, that represents the Jsonable. */
-    public String toJson();
+	/** Serialize to a JSON formatted string.
+	 * @return a string, formatted in JSON, that represents the Jsonable. */
+	public String toJson();
 
-    /** Serialize to a JSON formatted stream.
-     * @param writable where the resulting JSON text should be sent.
-     * @throws IOException when the writable encounters an I/O error. */
-    public void toJson(Writer writable) throws IOException;
+	/** Serialize to a JSON formatted stream.
+	 * @param writable where the resulting JSON text should be sent.
+	 * @throws IOException when the writable encounters an I/O error. */
+	public void toJson(Writer writable) throws IOException;
 }
